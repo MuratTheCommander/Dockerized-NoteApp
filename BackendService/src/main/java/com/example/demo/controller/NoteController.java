48736f2659bc1e332ee621b641b00f822d7c688e
MuratTheCommander <muratforbusiness58@gmail.com>
2026-01-11 19:48:16 +0300

    simplify the controller

--- BackendService/src/main/java/com/example/demo/controller/NoteController.java
+++ BackendService/src/main/java/com/example/demo/controller/NoteController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/note")
+@RequestMapping("/api")
 public class NoteController {
 
     @Autowired
