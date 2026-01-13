d07fedf403ff9a2ccbba0e6c0cb49097faae3e66
MuratTheCommander <muratforbusiness58@gmail.com>
2026-01-13 22:02:00 +0300

    fix the backend jwt filter

--- BackendService/src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
+++ BackendService/src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
@@ -1,26 +1,23 @@
 package com.example.demo.security;
 
-import io.jsonwebtoken.io.IOException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.util.List;
 
 
 @Component
-public class JwtAuthenticationFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    @Autowired
     private final SecurityService securityService;
 
     public JwtAuthenticationFilter(SecurityService securityService){
@@ -32,12 +29,13 @@ public class JwtAuthenticationFilter {
             HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain
-    ) throws ServletException, IOException, java.io.IOException {
+    ) throws ServletException, java.io.IOException {
 
     String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
 
     if(authHeader == null || !authHeader.startsWith("Bearer ")){
         filterChain.doFilter(request,response);
+        return;
     }
 
     String token = authHeader.substring(7);
@@ -46,8 +44,6 @@ public class JwtAuthenticationFilter {
         filterChain.doFilter(request,response);
         return;
     }
-
-    try{
 
         if(!securityService.validateJwt(token)){
             filterChain.doFilter(request,response);
@@ -69,12 +65,6 @@ public class JwtAuthenticationFilter {
 
         authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
         SecurityContextHolder.getContext().setAuthentication(authToken);
-
-    } catch (java.io.IOException e) {
-        throw new RuntimeException(e);
-    } catch (ServletException e) {
-        throw new RuntimeException(e);
-    }
 
     filterChain.doFilter(request,response);
 
